6b84fa17c8b4539d00530be454dd7f06a7a592b3
Albert G <albertag2002@gmail.com>
2024-04-20 17:32:16 +0530

    chat history added in app.py

--- app.py
+++ app.py
@@ -3,45 +3,110 @@ from dotenv import load_dotenv,find_dotenv
 from langchain_community.llms import HuggingFaceEndpoint
 from langchain_community.embeddings import SentenceTransformerEmbeddings
 from langchain_community.vectorstores import Chroma
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_community.document_loaders import PyMuPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 load_dotenv(find_dotenv())
 
 HF_TOKEN = os.environ.get("HUGGINGFACEHUB_API_TOKEN")
 
-embeddings = SentenceTransformerEmbeddings(model_name="all-mpnet-base-v2")
-vectorstore = Chroma(persist_directory="./chroma_db", embedding_function=embeddings)
+llm = HuggingFaceEndpoint(repo_id="mistralai/Mixtral-8x7B-Instruct-v0.1",max_new_tokens=1500,
+                                             temperature=0.5,top_k=80,top_p=0.95,streaming=True)
 
-retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 6})
+embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
 
-def format_docs(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
+print("-----------------------------------------------------------------------------")
 
-template = """You are a Stock Annual Report Question and Answer bot.
-You will be provided context to a company's annual report.
-Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say that you don't know, don't try to make up an answer.
-Use three sentences maximum and keep the answer as concise as possible.
+if os.path.exists('./chroma_db') == True:
+    print("DB Exists - Accessing......")
+    vectorstore = Chroma(persist_directory="./chroma_db", embedding_function=embeddings)
+else:
+    print("DB Does not Exist - Creating......")
+    loader = PyMuPDFLoader("data_pdfs/idfc_fy21.pdf")
+    data = loader.load()
 
-{context}
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=501000, chunk_overlap=1000)
+    all_splits = text_splitter.split_documents(data)
 
-Question: {question}
+    vectorstore = Chroma.from_documents(documents=all_splits, embedding=embeddings, persist_directory="./chroma_db")
 
-Helpful Answer:"""
+print("-----------------------------------------------------------------------------")
 
-custom_rag_prompt = PromptTemplate.from_template(template)
+retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 20})
 
-# mistralai/Mistral-7B-Instruct-v0.1/2
-# mistralai/Mixtral-8x7B-Instruct-v0.1
-llm = HuggingFaceEndpoint(repo_id="mistralai/Mistral-7B-Instruct-v0.2",max_new_tokens=1000,temperature=0.7,top_k=50,top_p=0.95)
-
-rag_chain = (
-    {"context": retriever | format_docs, "question": RunnablePassthrough()}
-    | custom_rag_prompt
-    | llm
-    | StrOutputParser()
+### Contextualize question ###
+contextualize_q_system_prompt = """Given a chat history and the latest user question \
+which might reference context in the chat history, formulate a standalone question \
+which can be understood without the chat history. Do NOT answer the question, \
+just reformulate it if needed and otherwise return it as is."""
+contextualize_q_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", contextualize_q_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+history_aware_retriever = create_history_aware_retriever(
+    llm, retriever, contextualize_q_prompt
 )
 
-print(rag_chain.invoke("Give me information on business ratios/information"))
+
+### Answer question ###
+qa_system_prompt = """You are an assistant for question-answering tasks. \
+Use the following pieces of retrieved context to answer the question. \
+If you don't know the answer, just say that you don't know. \
+Use three sentences maximum and keep the answer concise.\
+
+{context}"""
+qa_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", qa_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
+
+### Statefully manage chat history ###
+store = {}
+
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
+
+conversational_rag_chain = RunnableWithMessageHistory(
+    rag_chain,
+    get_session_history,
+    input_messages_key="input",
+    history_messages_key="chat_history",
+    output_messages_key="answer",
+)
+
+print("Welcome to the PDF Chatbot!")
+print("-----------------------------------------------------------------------------")
+
+i = 0
+while i == 0:
+    question = str(input("[1] Enter a question: \n[2] Enter 'Exit' to quit:\n"))
+    if question == 'Exit':
+        print("-x-x-x-x-x-x-x-x-x-x-x-")
+        print("Thank you!")
+        print("-x-x-x-x-x-x-x-x-x-x-x-")
+        break
+    else:
+        x1 = conversational_rag_chain.invoke({"input": question},
+                                             config={"configurable": {"session_id": "abc123"}},)
+        print(x1["answer"])
+        print("-----------------------------------------------------------------------------")
--- test_agent.py
+++ test_agent.py
@@ -1,12 +1,17 @@
-import os
 from langchain_community.embeddings import SentenceTransformerEmbeddings
 from langchain_community.document_loaders import PyMuPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain.chains import ConversationalRetrievalChain
+from langchain_community.llms import HuggingFaceEndpoint
+
+from langchain.globals import set_llm_cache
+from langchain.cache import InMemoryCache
+
+set_llm_cache(InMemoryCache())
+
 import os
 from dotenv import load_dotenv,find_dotenv
-from langchain_community.llms import HuggingFaceEndpoint
 
 load_dotenv(find_dotenv())
 
@@ -16,9 +21,12 @@ class Agent:
     def __init__(self, huggingfacehub_api_token: str | None = None) -> None:
         # if HUGGINGFACEHUB_API_TOKEN is None, then it will look the enviroment variable HUGGINGFACEHUB_API_TOKEN
         self.embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=501000, chunk_overlap=1000)
+        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=501000, chunk_overlap=20000)
+        # mistralai/Mistral-7B-Instruct-v0.1/2
+        # mistralai/Mixtral-8x7B-Instruct-v0.1
 
-        self.llm = llm = HuggingFaceEndpoint(repo_id="mistralai/Mistral-7B-Instruct-v0.2",max_new_tokens=1000,temperature=0.7,top_k=50,top_p=0.95)
+        self.llm = llm = HuggingFaceEndpoint(repo_id="mistralai/Mixtral-8x7B-Instruct-v0.1",max_new_tokens=1500,
+                                             temperature=0.5,top_k=80,top_p=0.95,streaming=True)
 
         self.chat_history = None
         self.chain = None
@@ -40,7 +48,8 @@ class Agent:
 
         if self.db is None:
             self.db = Chroma.from_documents(splitted_documents, self.embeddings)
-            self.chain = ConversationalRetrievalChain.from_llm(self.llm, self.db.as_retriever())
+            self.chain = ConversationalRetrievalChain.from_llm(self.llm, self.db.as_retriever(search_type="similarity", 
+                                                                                              search_kwargs={"k": 16}))
             self.chat_history = []
         else:
             self.db.add_documents(splitted_documents)
