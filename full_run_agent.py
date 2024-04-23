bbfc2bc0884867840da11047f469e65ce67a7690
Albert G <albertag2002@gmail.com>
2024-04-23 15:15:24 +0530

    done

--- full_run_agent.py
+++ full_run_agent.py
@@ -9,8 +9,8 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
-from langchain.retrievers.multi_query import MultiQueryRetriever
-from langchain_groq import ChatGroq
+# from langchain.retrievers.multi_query import MultiQueryRetriever
+# from langchain_groq import ChatGroq
 
 from langchain.globals import set_llm_cache
 from langchain.cache import InMemoryCache
@@ -25,26 +25,29 @@ load_dotenv(find_dotenv())
 HF_TOKEN = os.environ.get("HUGGINGFACEHUB_API_TOKEN")
 GROQ_TOKEN = os.environ.get("GROQ_API_KEY")
 
-groq_llm = ChatGroq(temperature=0, groq_api_key=GROQ_TOKEN, model_name="llama3-8b-8192")
+# groq_llm = ChatGroq(temperature=0, groq_api_key=GROQ_TOKEN, model_name="llama3-8b-8192")
 
 def clean_string(text):
     # Remove all occurrences of "AI:" and "</s>"
-    cleaned_text = text.replace("AI:", "").replace("</s>", "").replace("Assistant:","")
+    cleaned_text = text.replace("AI:", "").replace("</s>", "").replace("Assistant:","").replace("Bot:","")
     # Optionally, trim leading/trailing whitespace
     cleaned_text = cleaned_text.strip()
     return cleaned_text
 
 ### Contextualize question ###
-contextualize_q_system_prompt = """Given a chat history and the latest user question \
-which might reference context in the chat history, formulate a standalone question \
-which can be understood without the chat history. Do NOT answer the question, \
+contextualize_q_system_prompt = """Given a chat history and the latest user question
+which might reference context in the chat history, formulate a standalone question
+which can be understood without the chat history. Do NOT answer the question,
 just reformulate it if needed and otherwise return it as is."""
 
 ### Answer question ###
-qa_system_prompt = """You are an assistant for question-answering tasks. \
-Use the following pieces of retrieved context to answer the question. \
-If you don't know the answer, just say that you don't know. \
-Use three sentences maximum and keep the answer concise.\
+qa_system_prompt = """You are an Stock Annual Report Question and Answer bot.
+Answer with respect to the company's name.
+Use the following pieces of context to answer the question at the end.
+Perform calculations when needed and asked in the question.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+Use five sentences maximum and keep the answer as concise as possible.
+Always say "Thanks for asking!" at the end of the answer.
 
 {context}"""
 
@@ -54,7 +57,7 @@ class Agent:
     def __init__(self, huggingfacehub_api_token: str | None = None) -> None:
         # if HUGGINGFACEHUB_API_TOKEN is None, then it will look the enviroment variable HUGGINGFACEHUB_API_TOKEN
         self.embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=250000, chunk_overlap=10000)
+        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=501000, chunk_overlap=20000)
         # mistralai/Mistral-7B-Instruct-v0.1/2
         # mistralai/Mixtral-8x7B-Instruct-v0.1
 
@@ -67,8 +70,17 @@ class Agent:
         if self.chain is None:
             response = "Please, add a document."
         else:
-            response = self.chain.invoke({"input": question}, config={"configurable": {"session_id": "abc123"}},)
-            response = clean_string(response["answer"])
+            if question.endswith('[CONTEXT]'):
+                question = question.replace('[CONTEXT]','')
+                response = self.chain.invoke({"input": question}, config={"configurable": {"session_id": "abc123"}},)
+                context_response = ""
+                for document_p in response["context"]:
+                    context_response += str('PDF Page Number: '+str(document_p.metadata.get('page'))+'\n')
+                response = clean_string(response["answer"])+'\nCONTEXT OBTAINED FROM:\n'+context_response
+            else:
+                response = self.chain.invoke({"input": question}, config={"configurable": {"session_id": "abc123"}},)
+                response = clean_string(response["answer"])
+
         return response
 
     def ingest(self, file_path: os.PathLike) -> None:
@@ -83,11 +95,11 @@ class Agent:
             ("system", contextualize_q_system_prompt),
             MessagesPlaceholder("chat_history"),
             ("human", "{input}"),])
-            retrieev = self.db.as_retriever()
-            # secondary_retriever = self.db.as_retriever(search_type="similarity", search_kwargs={"k": 16})
-            # history_aware_retriever = create_history_aware_retriever(self.llm,secondary_retriever,contextualize_q_prompt)
-            history_aware_retriever = create_history_aware_retriever(
-            self.llm, MultiQueryRetriever.from_llm(retriever=retrieev,llm=groq_llm),contextualize_q_prompt)
+            # retrieev = self.db.as_retriever()
+            secondary_retriever = self.db.as_retriever(search_type="similarity", search_kwargs={"k": 16})
+            history_aware_retriever = create_history_aware_retriever(self.llm,secondary_retriever,contextualize_q_prompt)
+            # history_aware_retriever = create_history_aware_retriever(
+            # self.llm, MultiQueryRetriever.from_llm(retriever=retrieev,llm=groq_llm),contextualize_q_prompt)
 
             qa_prompt = ChatPromptTemplate.from_messages(
             [("system", qa_system_prompt),MessagesPlaceholder("chat_history"),("human", "{input}"),])
@@ -117,7 +129,7 @@ class Agent:
         self.chat_history = None
 
 # Set logging for the queries
-import logging
+# import logging
 
-logging.basicConfig()
-print(logging.getLogger("langchain.retrievers.multi_query").setLevel(logging.INFO))
+# logging.basicConfig()
+# print(logging.getLogger("langchain.retrievers.multi_query").setLevel(logging.INFO))
